a2c5cef23fd253351cba84a30287de32c725d8d2
Andrii <andrei.perwushin@gmail.com>
2023-03-27 14:54:09 +0300

    Page A4 landscape

---
+++ Sources/PdfBuilder/Helpers/CGRect + Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  CGRect + Extensions.swift
+//  
+//
+//  Created by Andrii Pervushyn on 27.03.2023.
+//
+
+import UIKit
+
+public extension CGRect {
+    static func landscapeA4() -> CGRect {
+        return CGRectMake(0.0, 0.0, 841.8, 595.2);
+    }
+}
